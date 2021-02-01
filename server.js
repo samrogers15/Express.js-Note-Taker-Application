af517a54e7ab635d14710ce16aed23f988b7b6cb
Sam Rogers <samrogers15@gmail.com>
2021-02-01 12:30:28 -0800

    committing updates to server.js code

--- server.js
+++ server.js
@@ -16,6 +16,7 @@
 const express = require('express');
 const path = require('path');
 const generateUniqueId = require('generate-unique-id');
+const fs = require('fs');
 
 // Create express app
 const app = express();
@@ -24,3 +25,83 @@ const PORT = 3000;
 // Set up Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('develop'));
+
+// Define landing page route
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './develop/public/index.html')));
+
+// Define notes page route
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './develop/public/notes..html')));
+
+// Define db.json path
+const dbJson = path.join(__dirname, 'db.json');
+
+// Displays all notes previously added to db.json
+app.get('/api/notes', (req, res) => res.sendFile(dbJson));
+
+// Create new notes - takes in JSON input
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    newNote.noteTitle = newNote.noteTitle.replace(/\s+/g, '').toLowerCase();
+    console.log(newNote);
+
+    dbJson.push(newNote);
+    res.json(newNote);
+});
+
+// Displays a single note, or returns false
+app.get('/api/notes/:title', (req, res) => {
+    const chosen = req.params.noteTitle
+    
+    console.log(chosen);
+
+    for (let i = 0; i < dbJson.length; i++) {
+        if (chosen === dbJson[i].noteTitle) {
+            return res.json(dbJson[i]);
+        };
+    };
+
+    return res.json(false);
+});
+
+// Create put route to update a previous note
+app.put('/api/notes/:title', (req, res) => {
+
+});
+
+// Create delete route to delete a specific note
+app.delete('/api/notes/:title', (req, res) => {
+
+});
+
+
+
+
+
+// Adds a new note to the db.json file
+// app.post('/api/notes', (req,res) => 
+//     fs.readFile(path.join(__dirname, 'db.json'), 'utf-8', (err, data) => {
+//     if (err) {
+//         console.log('Error inside the readFile code: ', err);
+//     } else {
+//         const noteRes = json.parse(res);
+//         const noteReq = req.body;
+//         const noteId = generateUniqueId();
+//         const newNote = {
+//             id = noteId,
+//             title = noteReq.title,
+//             text = noteReq.text
+//         }
+//         noteRes.push(newNote);
+//         res.json(newNote);
+//         fs.writeFile(path.join(__dirname, 'db.json'), 'utf-8', (err) => {
+//             if (err) {
+//                 console.log('Error inside the writeFile code: ', err);
+//                 throw err;
+//             }
+//         })
+//     }
+    
+//     });
+// );
