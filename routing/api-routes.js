2818d25e4d922a590e08b67420791298a4cde762
Sam Rogers <samrogers15@gmail.com>
2021-02-02 18:33:46 -0800

    committing working updates to all source code

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,4 +1,3 @@
-// const db = require('/../db/db.json');
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
@@ -6,47 +5,48 @@ const generateUniqueId = require('generate-unique-id');
 
 require('./html-routes');
 
-const id = generateUniqueId({
-    length: 10,
-    useLetters: true,
-    useNumbers: true
-});
-
 module.exports = function (app) {
-    fs.readFile('./db/db.json', 'utf8', function (err, data) {
+    fs.readFile('db/db.json', 'utf8', function (err, data) {
         if (err) throw err;
+        
         let notes = JSON.parse(data);
-
-        function updateNotes() {
-            fs.writeFile('./db/db.json', JSON.stringify(notes), function(error) {
-                if (err) throw err;
-                return true;
-            })
-        };
-
+      
         app.get('/api/notes', function(req, res) {
             res.json(notes);
         })
 
         app.post('/api/notes', function(req, res) {
-            // let activeNote = req.body;
+            
             const activeNote = {
                 title: req.body.title,
                 text: req.body.text,
-                id: id
+                id: generateUniqueId({
+                    length: 10,
+                    useLetters: true,
+                    useNumbers: true
+                })
             };
             notes.push(activeNote);
-            updateNotes();
+            updateNotes(notes);
+            res.json(notes);
         })
 
         app.get('/api/notes/:id', function(req, res) {
             res.json(notes[req.params.id]);
         })
 
-        // app.delete('/api/notes/:id', function(req, res) {
-        //     notes.splice(req.params.id, 1);
-        //     updateNotes();
-        // })
+        app.delete('/api/notes/:id', function(req, res) {
+            notes.splice(req.params.id, 1);
+            updateNotes(notes);
+            res.json(notes);
+        })
     })
+
+    function updateNotes(notes) {
+        fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), function(err) {
+            if (err) throw err;
+            return true;
+        })
+    };
 };
 
--- routing/html-routes.js
+++ routing/html-routes.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 module.exports = function (app) {
 
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, './public/index.html'));
+        res.sendFile(path.join(__dirname, '/../public/index.html'));
     });
 
     app.get('/notes', function(req, res) {
-        res.sendFile(path.join(__dirname, './public/notes.html'));
+        res.sendFile(path.join(__dirname, '/../public/notes.html'));
     });
 
 };
